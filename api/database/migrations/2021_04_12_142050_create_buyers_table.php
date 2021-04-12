e886519ddcf40f41a416b534096abf86148d8546
Daniel Betancur <daniel.betancur@evertecinc.com>
2021-04-12 14:46:33 -0500

    feat: migration for buyers is added

---
+++ api/database/migrations/2021_04_12_142050_create_buyers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBuyersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buyers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30);
+            $table->string('surname', 30);
+            $table->string('email', 50);
+            $table->string('mobile', 20);
+            $table->timestamps();
+
+            $table->foreignId('created_by')->nullable()->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buyers');
+    }
+}
