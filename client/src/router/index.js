547eb25e4ee0dbc3928073014c4e6f03cfb962c1
Daniel Betancur <daniel.betancur@evertecinc.com>
2021-04-13 10:56:22 -0500

    feat: necessary routes are added

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Buyers from "../views/Buyers.vue";
+import Tickets from "../views/Tickets";
+import RegisterBuyer from "../views/RegisterBuyer";
 
 Vue.use(VueRouter)
 
@@ -11,13 +14,20 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/buyers',
+    name: 'Buyers',
+    component: Buyers
+  },
+  {
+    path: '/tickets',
+    name: 'Tickets',
+    component: Tickets
+  },
+  {
+    path: '/register_buyer',
+    name: 'RegisterBuyer',
+    component: RegisterBuyer
+  },
 ]
 
 const router = new VueRouter({
